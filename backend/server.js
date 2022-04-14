3561fcfa1ebfcb47f337fec16b703261bf13dabf
Santiago Julian <sjulian983@gmail.com>
2022-04-14 15:22:14 +0200

    server created

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mg = require("mailgun-js");
+
+dotenv.config();
+
+const mailgun = () => {
+  mg({
+    apiKey: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN,
+  });
+};
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/api/email", (req, res) => {
+  const { email, subject, message } = req.body;
+  mailgun()
+    .message()
+    .send(
+      {
+        from: "SEJM <sejm@mg.yourdomain.com>",
+        to: `${email}`,
+        subject: `${subject}`,
+        html: `<p>${message}</p>`,
+      },
+      (err, body) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send({ message: "Error in sending the email" });
+        } else {
+          console.log(body);
+          res.send({ message: "Email sent succesfully" });
+        }
+      }
+    );
+});
